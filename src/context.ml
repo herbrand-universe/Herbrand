7dd74dd5061a43f3893f497d136bca738fca543e
Jacques Herbrand <fguido@localhost.(none)>
2012-05-16 03:40:24 -0300

    Remove debug message

--- src/context.ml
+++ src/context.ml
@@ -18,4 +18,4 @@ let inGlobal c n = List.mem_assoc n c.global
 
 let getGlobal c n = List.assoc n c.global
 
-let getLocal  c i = Format.printf "<%d>" (List.length c.local); List.nth c.local (i-1)
+let getLocal  c i = List.nth c.local (i-1)
