c6533bc98723c8976b68f0b878186f486a4841dc
Jacques Herbrand <fguido@localhost.(none)>
2012-05-16 03:37:11 -0300

    Fix a problem in context

--- src/context.ml
+++ src/context.ml
@@ -9,7 +9,7 @@ let empty () = {global = []; local = []}
 
 let addGlobal c n t = c.global <- (n , t) :: c.global; c
 
-let addLocal c t = c.local <- (t :: c.local); c
+let addLocal c t = { c with local = (t :: c.local)}
 
 let inLocal c i = i <= (List.length c.local)
 
@@ -18,4 +18,4 @@ let inGlobal c n = List.mem_assoc n c.global
 
 let getGlobal c n = List.assoc n c.global
 
-let getLocal  c i = List.nth c.local (i-1)
+let getLocal  c i = Format.printf "<%d>" (List.length c.local); List.nth c.local (i-1)
